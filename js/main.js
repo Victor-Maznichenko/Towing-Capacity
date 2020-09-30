364bbd6986e06088cce11d10239d99030feca8cb
Victor-Maznichenko <66438596+Victor-Maznichenko@users.noreply.github.com>
2020-09-30 21:59:09 +0300

    refdvc
    refvc

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+	$('.header__top').append($('<div class="header__menu-btn"><span></span><span></span><span></span></div>'));
 	
 	$('.header-item__select').selectize({
 		sortField: 'text'
@@ -13,4 +14,18 @@ $(function(){
 		$(this).toggleClass('trims__item-top--active');
 		$(this).siblings().slideToggle('slow');
 	});
+
+});
+
+var swiper = new Swiper('.swiper-container', {
+	loop: true,
+	slidesPerView: 'auto',
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'progressbar',
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 });
